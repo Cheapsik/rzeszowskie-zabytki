25c837309b903fb976a1ab9ba0808f3de544515b
Cheapsik <cheapsik@gmail.com>
2025-06-12 22:34:26 +0200

    fix: route fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,16 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Monument from "./pages/Monument";
 
 const App = () => {
 
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/zabytek/:id" element={<Monument />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   )
 }
 
